e9d2fc1df0c2e607bc4bbf53fffb93210fab88ee
Mahdiehmirzayi <149797919+Mahdiehmirzayi@users.noreply.github.com>
2026-02-19 02:14:23 +0330

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n ;
+    cout << "enter a number: ";
+    cin >> n;
+
+    bool isPrime = true ;
+
+    if (n<=1){
+        isPrime = false ;
+    }
+    else{
+        for (int i=2 ; i*i<=n ; i++){
+            if (n%i==0){
+                isPrime = false ;
+                break;
+            }
+        }
+    }
+
+    if (isPrime){
+        cout << "add aval ast" ;
+    }
+    else{
+        cout << "add aval nist" ;
+    }
+    return 0;
+}
---
+++ 5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n ;
+    cout << "enter a number:";
+    cin >> n ;
+
+    for(int num=2 ; num<n ; num++){
+        bool isPrime = true ;
+        for (int i=2 ; i*i<=num ; i++){
+            if (num%i==0){
+                isPrime = false ;
+                break;
+            }
+        }
+        if (isPrime){
+            cout << num << endl ;
+        }
+    }
+    return 0;
+}
